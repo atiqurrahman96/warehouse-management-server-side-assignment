4bf245c0f02afcd71ae9e44b527911e60ac7ae92
atiqurrahman96 <atiqurrahmanbd302@gmail.com>
2022-05-05 23:23:23 +0600

    password added

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 // -----connect with database-----
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://DB_USER:DB_PASSWORD@cluster0.yqit7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.yqit7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
     const collection = client.db("test").collection("devices");
