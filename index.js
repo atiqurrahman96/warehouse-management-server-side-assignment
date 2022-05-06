06f1059bd749396f8e8aec9cbb5d40e7fbd7943b
atiqurrahman96 <atiqurrahmanbd302@gmail.com>
2022-05-06 15:06:20 +0600

    get api added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
@@ -7,16 +8,38 @@ app.use(cors());
 app.use(express.json());
 // -----connect with database-----
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.yqit7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    console.log('db connected');
-    // perform actions on the collection object
-    client.close();
-});
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.yqit7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        const warehouseCollection = client.db('warehouse').collection('products');
+
+        //--------data load or data read----
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = warehouseCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+        // ----single data Load---------
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await warehouseCollection.findOne(query);
+            res.send(product);
+        })
+
+
+    }
+    finally {
+
+    }
+}
+
+
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
